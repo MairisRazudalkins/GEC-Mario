69483798ee3f483a3ddc69d8415df29147c87569
mairis13 <mairis1353@gmail.com>
2021-03-16 21:45:27 +0000

    Removal of old project

--- MarioBaseProject/MarioBaseProject/CharacterLuigi.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "BaseCharacter.h"
-class CharacterLuigi : public BaseCharacter
-{
-
-public:
-	CharacterLuigi(SDL_Renderer* renderer, std::string imagePath, Vector2D startPos, LevelMap* map);
-	void Update(float deltaSeconds, SDL_Event e) override;
-};
